6e9350d8196cbc6f75763a827d72876dc4e5419b
David Deutsch <skore@valanx.org>
2013-10-27 15:06:57 +0100

    working on table sync

--- deploy.php
+++ deploy.php
@@ -8,10 +8,6 @@ class TinyStage
 
 	public static function go()
 	{
-		if ( !file_exists( __DIR__.'/../config.json' ) ) {
-			header("HTTP/1.0 401 Unauthorized"); exit;
-		}
-
 		self::loadConfig();
 
 		self::checkAuth();
@@ -23,6 +19,10 @@ class TinyStage
 
 	public static function loadConfig()
 	{
+		if ( !file_exists( __DIR__.'/../config.json' ) ) {
+			header("HTTP/1.0 401 Unauthorized"); exit;
+		}
+
 		self::$config = json_decode( file_get_contents(__DIR__.'/../config.json') );
 	}
 
@@ -49,17 +49,19 @@ class TinyStage
 		$db_left = new PDO(
 			$dbc->left->dsn,
 			$dbc->left->user,
-			$dbc->left->password
+			$dbc->left->password,
+			array(PDO::ATTR_PERSISTENT => true)
 		);
 
 		$db_right = new PDO(
 			$dbc->right->dsn,
 			$dbc->right->user,
-			$dbc->right->password
+			$dbc->right->password,
+			array(PDO::ATTR_PERSISTENT => true)
 		);
 
-		$left = $db_left->query( 'show table status from'.$dbc->dbleft->db );
-		$right = $db_right->query( 'show table status from'.$dbc->dbright->db );
+		$left = $db_left->query( 'show table status from'.$dbc->left->db );
+		$right = $db_right->query( 'show table status from'.$dbc->right->db );
 
 		// Keep an offset in case right table has a reduced table set
 		$offset = 0;
@@ -88,7 +90,62 @@ class TinyStage
 				continue;
 			}
 
+			$q = $db_left->prepare('DESCRIBE '.$left_table['Name']);
+			$q->execute();
 
+			$table_fields = $q->fetchAll(PDO::FETCH_COLUMN);
+
+			$table_id = array_shift($table_fields);
+
+			// Prepare statements for selecting and inserting entries
+			$stmt_left = $db_left->prepare(
+				'SELECT * FROM '.$left_table['Name']
+			);
+
+			$stmt_right = $db_right->prepare(
+				'SELECT * FROM '.$left_table['Name']
+				.' WHERE '.$table_id.'=:'.$table_id
+			);
+
+			$inserts = array();
+			foreach ( $table_fields as $field ) {
+				$inserts[] = $field.'=:'.$field;
+			}
+
+			$stmt_update = $db_right->prepare(
+				'UPDATE '.$left_table['Name']
+				.' SET '.implode(', ', $inserts)
+			);
+
+			if (!$stmt_left->execute()) continue;
+
+			while ($left_row = $stmt_left->fetch(PDO::FETCH_ASSOC)) {
+				$stmt_right->bindValue(":".$table_id, $left_row[$table_id]);
+				$stmt_right->execute();
+
+				$right_row = $stmt_right->fetch(PDO::FETCH_ASSOC);
+
+				$same = true;
+				foreach ( $left_row as $j => $left_field ) {
+					if ( $right_row[$j] !== $left_field ) {
+						$same = false;
+
+						break;
+					}
+				}
+
+				if ( $same ) continue;
+
+				foreach ( $table_fields as $field ) {
+					$stmt_update->bindValue(":".$field, $field);
+				}
+
+				$stmt_update->execute();
+			}
 		}
+
+		// Close connections
+		$db_left = null;
+		$db_right = null;
 	}
 }
